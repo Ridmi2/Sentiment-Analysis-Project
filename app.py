d6a90327283e6ff3920427374767a77e4ef3bd45
Ridmi Tharaka Wickramasinghe <ridmit12@gmail.com>
2025-10-25 18:16:30 +0530

    build web application

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, redirect,render_template, request
+from helper import preprocessing,vectorizer,get_prediction
+
+from helper import get_prediction, vectorizer
+
+app = Flask(__name__)
+
+#as a trial how the data showing in the first page
+data =dict()
+reviews = []
+positive = 0
+negative = 0
+
+@app.route("/")
+def index():
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+    return render_template("index.html", data=data)
+
+@app.route("/", methods=['post'])
+def my_post():
+    text = request.form['text']
+    preprocessed_txt = preprocessing(text)
+    vectorized_txt = vectorizer(preprocessed_txt)
+    prediction = get_prediction(vectorized_txt)
+
+    if prediction == "negative":
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+
+    reviews.insert(0, text)  
+    return redirect(request.url)
+
+if __name__ == "__main__":
+    app.run()
